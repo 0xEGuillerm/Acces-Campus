d1ffca527b64dedc336b1c702a36be19c6231fe9
Emile Guillerm <81387146+0xEGuillerm@users.noreply.github.com>
2026-03-26 17:30:07 +0100

    v2 API
    +Ajout nouvelle route pour le BAE
    	Route BAE pour obtenir l'etat d'une salle avec les informations sur les élèves absents, le nombre d'élèves attendus.
    
    +Ajout de l'orodatage pour la route scanner badge de la BAE
    
    ~Modification de la propriété classe attendu par l'adresse MAC
    
    +Ajout du critère autorisee sur le components utilisateur pour signifier au BAE / PEA l'action clairement

---
+++ BDDEmile/Application/BackendBDD/controllers/ControlleurPSW.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Emile Guillerm on 22.03.2026.
+//
+
+#include "ControlleurPSW.h"
---
+++ BDDEmile/Application/BackendBDD/controllers/ControlleurPSW.h
@@ -0,0 +1,13 @@
+//
+// Created by Emile Guillerm on 22.03.2026.
+//
+
+#ifndef BACKENDBDD_CONTROLLEURPSW_H
+#define BACKENDBDD_CONTROLLEURPSW_H
+
+
+class ControlleurPSW {
+};
+
+
+#endif //BACKENDBDD_CONTROLLEURPSW_H
