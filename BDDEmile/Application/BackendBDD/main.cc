779983af53c6b5a583767ead134271d043ce3b78
Emile Guillerm <81387146+0xEGuillerm@users.noreply.github.com>
2026-04-07 20:51:12 +0200

    Version 1.1 Fonctionne sur mon PC Ajout d'un détécteur de configuration +Modifications

--- BDDEmile/Application/BackendBDD/main.cc
+++ BDDEmile/Application/BackendBDD/main.cc
@@ -1,8 +1,10 @@
 #include <drogon/drogon.h>
-int main() {
+#include <filesystem>
+
+int main(int argc, char* argv[]) {
     //Load config file
-    //drogon::app().loadConfigFile("../config.json");
-    drogon::app().loadConfigFile("../config.yaml");
+    auto configPath = std::filesystem::canonical(argv[0]).parent_path() / "config.json";
+    drogon::app().loadConfigFile(configPath.string());
     //Run HTTP framework,the method will block in the internal event loop
     drogon::app().run();
     return 0;
