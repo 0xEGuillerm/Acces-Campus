5c9cbc2865c55b5c20d32be856baa7124c529637
Emile Guillerm <81387146+0xEGuillerm@users.noreply.github.com>
2026-04-08 10:59:51 +0200

    Modification divers

--- BDDEmile/Application/BackendBDD/Logique/UtilisateurLogique.cpp
+++ BDDEmile/Application/BackendBDD/Logique/UtilisateurLogique.cpp
@@ -12,15 +12,22 @@
 
 using namespace drogon::orm;
 
+
+//Fonction de recherche utilisateur par prenom
 drogon::Task<Json::Value> UtilisateurLogique::UtilisateurParNomPrenom(
+    //Parametre nécessaire pour la recherche
     const DbClientPtr &db,
     const std::string &nom,
     const std::string &prenom) {
+    //Instanciation des variable pour la réponse format JSON
     Json::Value ReponseJSON;
     Json::Value listeprenom(Json::arrayValue);
     Json::Value listenom(Json::arrayValue);
+    //Verification de la présence de champ pour éviter les erreurs
     if (prenom != "") {
+        //Fonction couroutine pour la recherche avec la DAO
         auto ListeUtilisateurPrenom = co_await UtilisateurDAO::ChercherUtilisateurParPrenom(db, prenom);
+        //Verifciation et ajout des donné fournit apres la recherche dans le JSON
         for (const auto& utilisateur : ListeUtilisateurPrenom) {
             Json::Value user;
             user["id_utilisateur"] = (*utilisateur.getIdUser());
@@ -29,8 +36,10 @@ drogon::Task<Json::Value> UtilisateurLogique::UtilisateurParNomPrenom(
             listeprenom.append(user);
         }
     }
+    //Verification de la présence de champ pour éviter les erreurs
     if (nom != "") {
         auto ListeUtilisateurNom = co_await UtilisateurDAO::ChercherUtilisateurParNom(db, nom);
+        //Verifciation et ajout des donné fournit apres la recherche dans le JSON
         for (const auto& utilisateur : ListeUtilisateurNom) {
             Json::Value user;
             user["id_utilisateur"] = (*utilisateur.getIdUser());
@@ -41,5 +50,6 @@ drogon::Task<Json::Value> UtilisateurLogique::UtilisateurParNomPrenom(
     }
     ReponseJSON["Recherche_prenom"] = listeprenom;
     ReponseJSON["Recherche_nom"] = listenom;
+    //Retour de la coroutines
     co_return ReponseJSON;
 }
